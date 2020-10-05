a1627545ad9847c9234fb03a31a679a898e81314
Коваль Максим <67711236+Maksym-hub@users.noreply.github.com>
2020-10-06 00:16:12 +0300

    Api + PostgresSql
    CRUD aplication

---
+++ go-postgres/models/models.go
@@ -0,0 +1,10 @@
+package models
+
+
+// Table of users
+type User struct {
+	ID       int64  `json:"id"`
+	Name     string `json:"name"`
+	Location string `json:"location"`
+	Age      int64  `json:"age"`
+}
